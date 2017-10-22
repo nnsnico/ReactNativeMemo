3af9cfaa9cbcdf683161be385d87ecec2d383f8c
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-10-22 22:52:30 +0900

    Add list component

--- App.js
+++ App.js
@@ -1,57 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// Sample
+const sampleList = [
+  {
+    key: 'hoge',
+    title: 'hogeの神秘',
+    detail: 'hogeはすごいよ。とってもすごいんだよ。何よりもhogeしてhohohogegegeするのがすごい。やっぱすげぇわhogeは。ほら、そこの君もhogeのすごさを知ってきただろ？'
+  },
+  {
+    key: 'fuga',
+    title: 'fugaの宝具',
+    detail: 'fugaの宝具はfuga神によって5000兆年もの間守られ続けてきた神秘の宝である。形状は「fuga」の形をした禍々しい風格を持つ。一度手にしたものはfuga神の呪いによって永遠にふがふが言い続けるであろう'
+  },
+  {
+    key: 'piyo',
+    title: '伝説の島piyo島',
+    detail: 'piyopiyo!piyopiyopiyo!piyopiyopiyopiyopiyo!pipipipipipiiiiiiii!!!piyopiyopiyooooooooo!!!!!!!!\n二度と日本語がしゃべれなくなるぞ'
+  }
+];
 
-export default class App extends Component<{}> {
+class ListComponent extends Component {
   render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <FlatList
+        data={sampleList}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            key={item.key}
+            style={styles.listItem}
+            onPress={() => navigate('Detail', item)}
+          >
+            <Text style={styles.heading}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+        contentContainerStyle={styles.container}
+      />
+    );
+  }
+}
+
+class DetailComponent extends Component {
+  render() {
+    const {params} = this.props.navigation.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={[styles.heading, {marginBottom: 24}]}>{params.title}</Text>
+        <Text style={styles.paragraph}>{params.detail}</Text>
       </View>
     );
   }
 }
 
+export const SimpleApp = StackNavigator({
+  Detail: {screen: DetailComponent, navigationOptions: {title: 'Detail'}},
+  List: {screen: ListComponent, navigationOptions: {title: 'Home'}}
+}, {
+  initialRouteName: 'List'
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <SimpleApp/>
+    );
+  }
+}
+
+/* スタイル */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    paddingHorizontal: 16,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  heading: {
+    fontSize: 24,
+    color: 'rgba(14, 13, 13, .38)',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  paragraph: {
+    fontSize: 18,
+    color: '#737373'
+  },
+  listItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(14, 13, 13, .38)',
+    marginVertical: 12,
   },
 });
