296b9caafdf73b5b12f06da3fd9673c252a05b2c
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-10-24 14:12:53 +0900

    Add "Add to List" Component

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import {
   View,
   FlatList,
   TouchableOpacity,
+  Alert,
+  TouchableWithoutFeedback,
+  TextInput,
+  Button,
+  Keyboard,
 } from 'react-native';
 import {
   TabNavigator,
@@ -34,9 +39,11 @@ const sampleList = [
 class ListComponent extends Component {
   render() {
     const {navigate} = this.props.navigation;
+    const {screenProps} = this.props;
     return (
       <FlatList
-        data={sampleList}
+        data={screenProps.sampleList}
+        keyExtractor={(item, index) => index}
         renderItem={({item}) => (
           <TouchableOpacity
             key={item.key}
@@ -64,7 +71,7 @@ class DetailComponent extends Component {
   }
 }
 
-const Stack = StackNavigator({
+export const MemoListComponent = StackNavigator({
   Detail: {screen: DetailComponent, navigationOptions: {title: 'Detail'}},
   List: {screen: ListComponent, navigationOptions: {title: 'Home'}}
 }, {
@@ -72,25 +79,57 @@ const Stack = StackNavigator({
   headerMode: 'none'
 });
 
-class MemoListComponent extends Component {
-  render() {
-    return (
-      <Stack screenProps={this.screenProps}/>
-    );
-  }
-}
-
 class AddMemoItemComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: '', detail: ''};
+    this.handleOnPress = this.handleOnPress.bind(this);
+  }
+
+  handleOnPress() {
+    const {title, detail} = this.state;
+    if (!title) return Alert.alert('Error', 'titleは必須です');
+
+    this.props.screenProps.addNewMemoItem({title, detail});
+    Alert.alert(
+      'Notice',
+      '項目を追加しました',
+      [{text: 'OK', onPress: () => this.props.navigation.navigate('List') }],
+    );
+    this.setState({title: '', detail: ''});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>This is AddMemoItemScreen</Text>
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.inputGroup}>
+            <Text style={[styles.paragraph, styles.label]}>title</Text>
+            <TextInput
+              blurOnSubmit
+              onChangeText={title => this.setState({title})}
+              style={[styles.textInput, styles.heading]}
+              value={this.state.title}
+            />
+          </View>
+          <View style={styles.inputGroup}>
+            <Text style={[styles.paragraph, styles.label]}>detail</Text>
+            <TextInput
+              blurOnSubmit
+              multiline
+              onChangeText={detail => this.setState({detail})}
+              style={[styles.multiTextInput, styles.paragraph]}
+              value={this.state.detail}
+            />
+          </View>
+          <Button onPress={this.handleOnPress} title={'Add item to list'}/>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
-const Tab = TabNavigator({
+export const Tab = TabNavigator({
   List: {
     screen: MemoListComponent,
     navigationOptions: {
@@ -124,16 +163,23 @@ export default class App extends Component<{}> {
     this.state = {
       sampleList,
     };
+    this.addNewMemoItem = this.addNewMemoItem.bind(this);
+  }
+
+  addNewMemoItem({title, detail}) {
+    this.setState({
+      sampleList: [...this.state.sampleList, {title, detail}],
+    });
   }
 
   render() {
     return (
-      <Tab screenProps={{sampleList: this.state.sampleList}}/>
+      <Tab screenProps={{sampleList: this.state.sampleList, addNewMemoItem: this.addNewMemoItem}}/>
     );
   }
 }
 
-/* スタイル */
+/* style */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -154,5 +200,29 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: 'rgba(14, 13, 13, .38)',
     marginVertical: 12,
+  },
+  inputGroup: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+  },
+  label: {
+    minWidth: 60,
+  },
+  textInput: {
+    flex: 4,
+    borderColor: 'rgba(14, 13, 13, .38)',
+    borderWidth: 1,
+    paddingHorizontal: 12,
+    height: 40,
+  },
+  multiTextInput: {
+    flex: 4,
+    borderColor: 'rgba(14, 13, 13, .38)',
+    borderWidth: 1,
+    paddingHorizontal: 9,
+    height: 80,
   },
 });
