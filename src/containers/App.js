d9c12125a266231f2dc9f3c199b02341c1c6bddb
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-12-05 16:13:30 +0900

    Add Initial Memo on Reducer and Refactor code

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,24 +6,6 @@ import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 import DetailScreen from '../components/DetailScreen';
 import HomeScreen from '../components/HomeScreen';
 
-export const sampleList = [
-  {
-    key: 'hoge',
-    title: 'hogeの神秘',
-    detail: 'hogeはすごいよ。とってもすごいんだよ。何よりもhogeしてhohohogegegeするのがすごい。やっぱすげぇわhogeは。ほら、そこの君もhogeのすごさを知ってきただろ？',
-  },
-  {
-    key: 'fuga',
-    title: 'fugaの宝具',
-    detail: 'fugaの宝具はfuga神によって5000兆年もの間守られ続けてきた神秘の宝である。形状は「fuga」の形をした禍々しい風格を持つ。一度手にしたものはfuga神の呪いによって永遠にふがふが言い続けるであろう',
-  },
-  {
-    key: 'piyo',
-    title: '伝説の島piyo島',
-    detail: 'piyopiyo!piyopiyopiyo!piyopiyopiyopiyopiyo!pipipipipipiiiiiiii!!!piyopiyopiyooooooooo!!!!!!!!\n二度と日本語がしゃべれなくなるぞ',
-  },
-];
-
 export const AppNavigator = StackNavigator({
   // 詳細画面
   Detail: {
@@ -44,6 +26,7 @@ function AppWithNavigationState({ dispatch, nav }) {
   return (
     <AppNavigator
       navigation={addNavigationHelpers({ dispatch, state: nav })}
+      screenProps={{ dispatch }}
     />
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { NavigationActions } from 'react-navigation';
 import { combineReducers } from 'redux';
 import { AppNavigator } from '../containers/App';
 
-const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Home'));
+const initialNavState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Home'));
 
-function nav(state = initialState, action) {
+function nav(state = initialNavState, action) {
   switch (action.type) {
     case 'GO_DETAIL': {
       return (
@@ -23,8 +23,44 @@ function nav(state = initialState, action) {
   }
 }
 
+const initialMemoList = [
+  {
+    key: 'hoge',
+    title: 'hogeの神秘',
+    detail: 'hogeはすごいよ。とってもすごいんだよ。何よりもhogeしてhohohogegegeするのがすごい。やっぱすげぇわhogeは。ほら、そこの君もhogeのすごさを知ってきただろ？',
+  },
+  {
+    key: 'fuga',
+    title: 'fugaの宝具',
+    detail: 'fugaの宝具はfuga神によって5000兆年もの間守られ続けてきた神秘の宝である。形状は「fuga」の形をした禍々しい風格を持つ。一度手にしたものはfuga神の呪いによって永遠にふがふが言い続けるであろう',
+  },
+  {
+    key: 'piyo',
+    title: '伝説の島piyo島',
+    detail: 'piyopiyo!piyopiyopiyo!piyopiyopiyopiyopiyo!pipipipipipiiiiiiii!!!piyopiyopiyooooooooo!!!!!!!!\n二度と日本語がしゃべれなくなるぞ',
+  },
+];
+
+function memo(state = initialMemoList, action) {
+  switch (action.type) {
+    case 'ADD_MEMO': {
+      return [
+        ...state,
+        {
+          title: action.title,
+          detail: action.detail,
+        },
+      ];
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
 const AppReducer = combineReducers({
-  nav: nextState = nav,
+  nav,
+  memo,
 });
 
 export default AppReducer;
