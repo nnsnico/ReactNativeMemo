24efbd8e05ce8d904b34734a2f0baf0db31a56f3
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-01-15 03:05:46 +0900

    Add MemoListItem.tsx

--- src/components/ListScreen.tsx
+++ src/components/ListScreen.tsx
@@ -1,44 +1,28 @@
 import * as React from 'react';
 import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
 
-import { goDetail } from '../actions/index';
+import MemoListItem from './MemoListItem';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 16,
-  },
-  item: {
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(14, 13, 13, .38)',
-    marginVertical: 12,
-  },
-  heading: {
-    fontSize: 24,
-    color: 'rgba(14, 13, 13, .38)',
   },
 });
 
 
 class ListScreen extends React.Component<any, any> {
   render() {
-    const { navigation, memo } = this.props.screenProps;
-    console.log(1, memo);
+    const { goDetailScreen, memo } = this.props.screenProps;
     return (
       <FlatList
         data={memo}
+        extraData={memo}
         keyExtractor={(item: any, index: number) => String(index)}
         renderItem={({ item }) => (
-          <TouchableOpacity
-            key={item.key}
-            style={styles.item}
-            onPress={() => navigation.dispatch(goDetail(item))}
-          >
-            <Text style={styles.heading}>{item.title}</Text>
-          </TouchableOpacity>
+          <MemoListItem
+            item={item}
+            goDetailScreen={goDetailScreen}
+          />
         )}
         contentContainerStyle={styles.container}
       />
