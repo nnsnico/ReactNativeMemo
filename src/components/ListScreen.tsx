b0d8d563b7db25d245b3c8e9898a04860cfb78a6
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-01-26 01:55:09 +0900

    Add not found memo screen

--- src/components/ListScreen.tsx
+++ src/components/ListScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, FlatList, TouchableOpacity, View } from 'react-native';
 
 import MemoListItem from './MemoListItem';
 import Memo from '../models/Memo';
@@ -8,6 +8,15 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
   },
+  no_data_container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  no_data_text: {
+    fontSize: 30,
+  }
 });
 
 interface ScreenPropsProperties {
@@ -20,21 +29,52 @@ interface ListScreenPropaties {
 }
 
 class ListScreen extends React.Component<ListScreenPropaties, any> {
+  constructor(props: ListScreenPropaties) {
+    super(props);
+    this.screen = this.screen.bind(this);
+  }
+
+  screen = () => {
+    const { goDetailScreen, memo } = this.props.screenProps;
+    if (memo.length === 0) {
+      return <Text>No List</Text>
+    } else {
+      return
+
+    }
+  }
+
   render() {
     const { goDetailScreen, memo } = this.props.screenProps;
-    return (
-      <FlatList
-        data={memo}
-        extraData={memo}
-        keyExtractor={(item: any, index: number) => String(index)}
-        renderItem={({ item }) => (
-          <MemoListItem
-            item={item}
-            goDetailScreen={goDetailScreen}
+    const Screen = function (memo: Memo[]) {
+      if (memo.length === 0) {
+        return (
+          <View style={styles.no_data_container}>
+            <Text style={styles.no_data_text}>No data</Text>
+          </View>
+        )
+      } else {
+        return (
+          <FlatList
+            data={memo}
+            extraData={memo}
+            keyExtractor={(item: any, index: number) => String(index)}
+            renderItem={({ item }) => (
+              <MemoListItem
+                item={item}
+                goDetailScreen={goDetailScreen}
+              />
+            )}
+            contentContainerStyle={styles.container}
           />
-        )}
-        contentContainerStyle={styles.container}
-      />
+        )
+      }
+    }
+
+    return (
+      <View style={[styles.container, { flex: (memo.length === 0) ? 1 : 0 }]}>
+        {Screen(memo)}
+      </View>
     );
   }
 }
