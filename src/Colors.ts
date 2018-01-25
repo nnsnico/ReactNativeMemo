4644ce8729a5e348c80c1415fb3be0a54054ac6c
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-01-26 03:14:31 +0900

    Add Color Sheme

---
+++ src/Colors.ts
@@ -0,0 +1,7 @@
+enum Colors {
+  PRIMARY_DARK = '#9575CD',   // Deep purple 300
+  PRIMARY = '#B39DDB',        // Deep purple 100
+  ACCENT = '#F06292',         // Pink 300
+}
+
+export default Colors;
