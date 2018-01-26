ccc41f0f42628dfb24f9c2f51d0c98ba2727dc3e
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-01-26 18:15:30 +0900

    Add remove by AsyncStorage and Saga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,7 +1,7 @@
 import { put, takeEvery, all, call, select, take, fork, SelectEffect } from 'redux-saga/effects'
 
 import LocalStorage from '../data/LocalStorage';
-import { ADD_MEMO_PROPERTIES, addMemo } from '../actions/index';
+import { ADD_MEMO_PROPERTIES, addMemo, REMOVE_MEMO_PROPERTIES, removeMemo } from '../actions/index';
 import Memo from '../models/Memo';
 
 function* initialLoadStorage() {
@@ -19,13 +19,23 @@ function* addMemoAsync(action: ADD_MEMO_PROPERTIES) {
   yield put(addMemo(action.memo));
 }
 
+function* removeMemoAsync(action: REMOVE_MEMO_PROPERTIES) {
+  yield LocalStorage.removeItem(action.memo);
+  yield put(removeMemo(action.memo));
+}
+
 function* watchAddMemoAsync() {
   yield takeEvery('ADD_MEMO_ASYNC', addMemoAsync)
+}
+
+function* watchRemoveMemoAsync() {
+  yield takeEvery('REMOVE_MEMO_ASYNC', removeMemoAsync)
 }
 
 export default function* rootSaga() {
   yield fork(initialLoadStorage);
   yield all([
-    watchAddMemoAsync()
+    watchAddMemoAsync(),
+    watchRemoveMemoAsync(),
   ]);
 }
